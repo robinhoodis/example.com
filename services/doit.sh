174b6dbce1f2af401905d3b9dcb78d87fa491577
robinhoodis <f5examplecom@gmail.com>
2018-06-05 19:25:34 -0400

    creating build scripts

--- services/doit.sh
+++ services/doit.sh
@@ -11,21 +11,20 @@
 #docker volume create HTTPD_DATA_PATH
 
 DOMAIN="example.com"
-declare -a SITES
-declare -a SERVICES
-SITES=( [site1]="10.1.30" [site2]="10.1.40" ) 
-SERVICES=( [ftp]='--mount source=HOME,target=/home --mount source=FTPLOGS,target=/var/log/vsftpd' [http]='--mount source=HOME,target=/home --mount source=HTTPD_DATA_PATH,target=/var/www --mount source=HTTPLOGS,target=/var/log/httpd' [dns]='--volume /srv/docker/bind9:/named' )
+#declare -a SITES
+#declare -a SERVICES
+declare -A SITES=( [site1]="10.1.30" [site2]="10.1.40" ) 
+declare -A SERVICES=( [ftp]='--mount source=HOME,target=/home --mount source=FTPLOGS,target=/var/log/vsftpd' [http]='--mount source=HOME,target=/home --mount source=HTTPD_DATA_PATH,target=/var/www --mount source=HTTPLOGS,target=/var/log/httpd' [dns]='--volume /srv/docker/bind9:/named' )
 
 i=7
 for SERVICENAME in "${!SERVICES[@]}"
   do
     echo "docker build -t examplecom/${SERVICENAME} -f Dockerfile.${SERVICENAME} ."
-exit
     docker build -t examplecom/${SERVICENAME} -f Dockerfile.${SERVICENAME} .
     docker push examplecom/${SERVICENAME}
     for SITENAME in ${!SITES[@]}; do
       for CONTAINER in 1 2 3; do
-        docker stop ${SITENAME}.${SERVICENAME}${CONTAINER}
+        docker stop ${SITENAME}.${SERVICENAME}${CONTAINER} || true
         echo "docker run -d --name ${SITENAME}.${SERVICENAME}${CONTAINER} --hostname ${SITENAME}.${SERVICENAME}${CONTAINER}.${DOMAIN} --net ${SITENAME} --ip ${SITES[$SITENAME]}.${i}${CONTAINER} --rm ${SERVICES[$SERVICENAME]} examplecom/${SERVICENAME}"
         docker run -d --name ${SITENAME}.${SERVICENAME}${CONTAINER}  --hostname ${SITENAME}.${SERVICENAME}${CONTAINER}.${DOMAIN} --net ${SITENAME} --ip ${SITES[$SITENAME]}.${i}${CONTAINER} --rm ${SERVICES[$SERVICENAME]} examplecom/${SERVICENAME}
      done
